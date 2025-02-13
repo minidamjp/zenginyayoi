34651517b65a8d97e8b486afe17b5d0e42a48006
minidam <minidamjp@ikedam.jp>
2025-02-13 22:17:53 +0900

    SJISに変換してダウンロードするコードを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import Encoding from 'encoding-japanese';
+import FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'zenginyayoi';
+  content = '';
+  download() {
+    const sjis = new Uint8Array(Encoding.convert(Encoding.stringToCode(this.content), 'SJIS'));
+    const data = new Blob([sjis], { type: 'text/plain;charset=shift_jis' });
+    FileSaver.saveAs(data, 'text.txt');
+  }
 }
